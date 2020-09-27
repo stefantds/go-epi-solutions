758764f57e649adfc370f5779c08dbfd2a955685
Stefan Tudose <stefan.tudose@data4life.care>
2020-09-27 18:35:15 +0200

    solve phone-mnemonic

--- epi/phone_number_mnemonic/solution.go
+++ epi/phone_number_mnemonic/solution.go
@@ -1,6 +1,44 @@
 package phone_number_mnemonic
 
+import (
+	"strconv"
+)
+
 func PhoneMnemonic(phoneNumber string) []string {
-	// TODO - Add your code here
-	return nil
+	mapping := map[int]string{
+		0: "0",
+		1: "1",
+		2: "ABC",
+		3: "DEF",
+		4: "GHI",
+		5: "JKL",
+		6: "MNO",
+		7: "PQRS",
+		8: "TUV",
+		9: "WXYZ",
+	}
+
+	return mnemonicHelper(phoneNumber, mapping)
+}
+
+func mnemonicHelper(phoneNumber string, mapping map[int]string) []string {
+	if len(phoneNumber) == 0 {
+		return []string{""}
+	}
+
+	mnemonics := mnemonicHelper(string(phoneNumber[1:]), mapping)
+	currentDigit, err := strconv.Atoi(string(phoneNumber[0]))
+	if err != nil {
+		panic(err)
+	}
+
+	result := make([]string, 0)
+
+	for _, m := range mnemonics {
+		for _, c := range mapping[currentDigit] {
+			result = append(result, string(c)+m)
+		}
+	}
+
+	return result
 }
