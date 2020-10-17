c04b59eb05e862e04b2a1311a730f622b6412ee7
Stefan Tudose <stefan.tudose@data4life.care>
2020-10-17 14:44:37 +0200

    solve 13.5 and 14.4

--- epi/lowest_common_ancestor_in_bst/solution.go
+++ epi/lowest_common_ancestor_in_bst/solution.go
@@ -4,7 +4,26 @@ import (
 	"github.com/stefantds/go-epi-judge/tree"
 )
 
-func FindLCA(tree *tree.BSTNode, s *tree.BSTNode, b *tree.BSTNode) *tree.BSTNode {
-	// TODO - Add your code here
-	return nil
+func FindLCA(t *t.BSTNode, s *tree.BSTNode, b *tree.BSTNode) *tree.BSTNode {
+	if t == nil { // only happens if no node satisfies the condition
+		return nil
+	}
+
+	var lca *t.BSTNode
+	switch {
+	case s == t:
+		lca = s
+	case b == t:
+		lca = b
+	case t.Data > s.Data && t.Data < b.Data:
+		lca = t
+	case t.Data < s.Data && t.Data > b.Data:
+		lca = t
+	case t.Data > s.Data: // both searched nodes are left
+		lca = FindLCA(t.Left, s, b)
+	case t.Data < s.Data: // both searched nodes are right
+		lca = FindLCA(t.Right, s, b)
+	}
+
+	return lca
 }
--- epi/smallest_nonconstructible_value/solution.go
+++ epi/smallest_nonconstructible_value/solution.go
@@ -1,6 +1,20 @@
 package smallest_nonconstructible_value
 
+import (
+	"sort"
+)
+
 func SmallestNonconstructibleValue(a []int) int {
-	// TODO - Add your code here
-	return 0
+	sort.Ints(a)
+	smallestNC := 1
+
+	for _, v := range a {
+		if v <= smallestNC {
+			// all values smaller than smallestNC can be created, so by adding v all values until
+			// smallestNC+v can be constructed as well.
+			smallestNC += v
+		}
+	}
+
+	return smallestNC
 }
