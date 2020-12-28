c97ee3b5ea7cf9d4b0fa63aa65c6ed5c351399cb
Stefan Tudose <stefan.tudose@data4life.care>
2020-12-28 21:08:04 +0100

    add solutions for 6.7 6.8

--- epi/look_and_say/solution.go
+++ epi/look_and_say/solution.go
@@ -1,6 +1,42 @@
 package look_and_say
 
+import (
+	"strconv"
+	"strings"
+)
+
 func LookAndSay(n int) string {
-	// TODO - Add your code here
-	return ""
+	current := "1"
+	for i := 1; i < n; i++ {
+		current = lookAndSayPrevious(current)
+	}
+
+	return current
+}
+
+func lookAndSayPrevious(input string) string {
+	var resultBuilder strings.Builder
+	inputChars := []rune(input)
+	if len(inputChars) == 0 {
+		return ""
+	}
+
+	prevChar := inputChars[0]
+	count := 1
+
+	for i := 1; i < len(inputChars); i++ {
+		if inputChars[i] == prevChar {
+			count++
+		} else {
+			resultBuilder.WriteString(strconv.Itoa(count)) // write the count
+			resultBuilder.WriteRune(prevChar)              // write the character counted
+			count = 1
+			prevChar = inputChars[i]
+		}
+	}
+
+	resultBuilder.WriteString(strconv.Itoa(count)) // write the count
+	resultBuilder.WriteRune(prevChar)              // write the character counted
+
+	return resultBuilder.String()
 }
--- epi/roman_to_integer/solution.go
+++ epi/roman_to_integer/solution.go
@@ -1,6 +1,47 @@
 package roman_to_integer
 
+import "fmt"
+
+type romanDigit rune
+
+const (
+	I romanDigit = 'I'
+	V romanDigit = 'V'
+	X romanDigit = 'X'
+	L romanDigit = 'L'
+	C romanDigit = 'C'
+	D romanDigit = 'D'
+	M romanDigit = 'M'
+)
+
+var values = map[romanDigit]int{
+	I: 1,
+	V: 5,
+	X: 10,
+	L: 50,
+	C: 100,
+	D: 500,
+	M: 1000,
+}
+
 func RomanToInteger(s string) int {
-	// TODO - Add your code here
-	return 0
+	result := 0
+	previousValue := 0
+	chars := []rune(s)
+	for i := len(chars) - 1; i >= 0; i-- {
+		digit := chars[i]
+		value, ok := values[romanDigit(digit)]
+		if !ok {
+			panic(fmt.Errorf("expected a valid value, got %s", s))
+		}
+
+		if value < previousValue {
+			result = result - value
+		} else {
+			result = result + value
+		}
+		previousValue = value
+	}
+
+	return result
 }
