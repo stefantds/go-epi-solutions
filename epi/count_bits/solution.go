8f02af40b6a670753774979fd3d65fb1241ef476
Stefan Tudose <stefan.tudose@data4life.care>
2021-02-17 19:41:02 +0100

    add solution for 4.0

--- epi/count_bits/solution.go
+++ epi/count_bits/solution.go
@@ -1,6 +1,22 @@
 package count_bits
 
-func CountBits(x int) int16 {
-	// TODO - Add your code here
-	return 0
+func CountBits(x int) int {
+	counter := 0
+	for x != 0 {
+		counter += x & 1
+		x = x >> 1
+	}
+
+	return counter
+}
+
+// CountBitsV2 uses the lowest set bit to skip zero bits
+func CountBitsV2(x int) int {
+	counter := 0
+	for x != 0 {
+		counter += 1
+		x = x & (x - 1) // drop lowest set bit of x
+	}
+
+	return counter
 }
